448a835a277bd373a7f010ea29413eaf47aebbbb
Shirley Alvarado <shirley@codecrc.com>
2021-03-17 15:52:09 -0600

    Retweet added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   resources :tweets do
     resources :likes
+    member do
+      post 'create_rt'
+    end
   end
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
   root to: "tweets#index"
 end
+
+
+
