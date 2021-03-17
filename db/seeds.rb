19503a511d1e936028dddbd5e28b98c08da14528
Shirley Alvarado <shirley@codecrc.com>
2021-03-17 11:24:15 -0600

    Seed changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-u = User.create(name: 'Juan', email: 'juan@example.com', password: '123123')
-
-100.times do |i|
-  Tweet.create(content: "Contenido #{i}")
-end
-
-{
-  "created_at": "Mon Dec 10 00:00:00 +0000 2012",
-  "id": 8821452687517076614,
-  "id_str": "8821452687517076614",
-  "text": "Ea et laboriosam vel non.",
-  // ...
-}
-
-25.times do 
-  user = User.create(name: Faker::Music::RockBand.name,
-              kind: [0,1].sample,
-  puts "Created user: #{user.name}" 
-
-  (10..100).to_a.sample.times do |i|
-  concert = band.concerts.build(location: Faker::Address.city,
-                          date: Faker::Date.between(from:'2019-01-01', to: Date.today),
-                          quantity: (1000..25000).to_a.sample,
-                          duration: (1..4).to_a.sample)
-      concert.save
-      puts "Concert in: #{concert.location}"
+25.times do |i|
+  user = User.create(name: Faker::Twitter.name, email: "example#{i}@example.com", password: '123456')
+  
+  4.times do |i|
+    tweet = Tweet.create(content:"Contenido#{i}"), user_id: user.id)
+  
+    tweet.save
+    puts "Tweet in: #{tweet.content}"
   end
 end
